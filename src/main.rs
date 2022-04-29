0b64b66940230e4f8d5e24e30df88521e3c658f5
mhnaufal <aal.pirates16@gmail.com>
2022-04-29 22:46:48 +0700

    add dynamic port

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 // ! src/main.rs
 
-use std::net::TcpListener;
+use std::{env, net::TcpListener};
 
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
+    let p = env::var("PORT")
+        .unwrap_or_else(|_| 3000.to_string())
+        .parse::<u16>()
+        .unwrap();
+
+    let dynamic_address = format!("0.0.0.0:{}", p);
+
+    let address = TcpListener::bind(dynamic_address)?;
     let port = address.local_addr().unwrap().port();
 
     // call the run() method in lib.rs and await for the connection
